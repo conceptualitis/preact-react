db52d4c67d3d9034b80bd2df636a61d0406141d8
Neil Duffy <neiladuffy@gmail.com>
2017-07-11 11:02:51 -0500

    fake messages

--- react-example/src/reducers/messages.js
+++ react-example/src/reducers/messages.js
@@ -4,7 +4,22 @@ const initialState = [
   {
     title: 'Hello friend',
     from: 'Bob',
-    body: 'It was great seeing you again!'
+    body: 'Arrived compass prepare an on as. Reasonable particular on my it in sympathize. Size now easy eat hand how. Unwilling he departure elsewhere dejection at. Heart large seems may purse means few blind. Exquisite newspaper attending on certainty oh suspicion of. He less do quit evil is. Add matter family active mutual put wishes happen.'
+  },
+  {
+    title: 'Hey',
+    from: 'Roger',
+    body: 'He difficult contented we determine ourselves me am earnestly. Hour no find it park. Eat welcomed any husbands moderate. Led was misery played waited almost cousin living. Of intention contained is by middleton am. Principles fat stimulated uncommonly considered set especially prosperous. Sons at park mr meet as fact like.'
+  },
+  {
+    title: 'Re: funding',
+    from: 'Becky',
+    body: 'Saw yet kindness too replying whatever marianne. Old sentiments resolution admiration unaffected its mrs literature. Behaviour new set existence dashwoods. It satisfied to mr commanded consisted disposing engrossed. Tall snug do of till on easy. Form not calm new fail.'
+  },
+  {
+    title: 'Lol',
+    from: 'Dave',
+    body: 'As collected deficient objection by it discovery sincerity curiosity. Quiet decay who round three world whole has mrs man. Built the china there tried jokes which gay why. Assure in adieus wicket it is. But spoke round point and one joy. Offending her moonlight men sweetness see unwilling. Often of it tears whole oh balls share an. '
   }
 ];
 
